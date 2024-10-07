be2f2fc599369767e75ac966aba476f2fad9de84
Marko489 <marek.jokay@gmail.com>
2024-10-07 10:01:54 +0200

    NavBar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 
 
-
 import type { Metadata } from "next";
 import "./globals.css";
-
+import Navbar from "@/components/NavBar";
 
 
 export const metadata: Metadata = {
-  title: "Snap Zoska",
-  description: "Generatated by students in SPSE Zochova 9, Bratislava",
+  title: "SnapZoška",
+  description: "Created by students of SPŠE Zochova 9, Bratislava",
 };
 
 export default function RootLayout({
@@ -19,7 +18,12 @@ export default function RootLayout({
   return (
     <html lang="sk">
       <body>
-        {children}
+        <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+          <main style={{ flexGrow: 1 }}>
+            {children}
+          </main>
+          <Navbar /> {/* Bottom Navigation */}
+        </div>
       </body>
     </html>
   );
